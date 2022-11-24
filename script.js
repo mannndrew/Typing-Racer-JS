5673a0c56dda412b73c0d07322edc9cda9c91317
mannndrew <mannndrew@gmail.com>
2022-11-23 21:58:21 -0600

    Loop

--- script.js
+++ script.js
@@ -13,97 +13,105 @@ var previousCorrectNum = 0
 var totalWordsTyped = 0
 var totalSentencesCompleted = 0
 var seconds = 0;
+var checkNextWord = true;
+
 
 quoteInputElement.addEventListener('input', () => {
 
-    
-    let correct = []
-    let incorrect = []
-    let unTyped = quote.split('')
+    do
+    {
+        let correct = []
+        let incorrect = []
+        let unTyped = quote.split('')                               //Split the un-typed portion to compare against input.
 
-    const Input = quoteInputElement.value.split('')
-    let correctNum = 0;
-    let complete = false
-    
-    
-    for (let i=0; i<Input.length; i++) {
-        const character = Input[i]
+        const Input = quoteInputElement.value.split('')             //Split user input to array to check against un-typed.
+        let correctNum = 0;
+        let complete = false
+        checkNextWord = false;
 
-        if (character == quote[i] && i==correctNum) {
-            unTyped[i] = ''
-            correct[i] = quote[i]
-            correctNum++
+        for (let i=0; i<Input.length; i++) {                        //Loop over user input, move each un-typed to either correct or incorrect arrays.
+            const character = Input[i]
+
+            if (character == quote[i] && i==correctNum) {
+                unTyped[i] = ''
+                correct[i] = quote[i]
+                correctNum++
+            }
+
+            else {
+                unTyped[i] = ''
+                incorrect[i] = quote[i]
+            }
         }
 
-        else {
-            unTyped[i] = ''
-            incorrect[i] = quote[i]
+        if (wordLength+1 <= correctNum && Input[wordLength] == ' ') {   // Execute if correct word, but not last.
+        
+            previousCorrectWords = previousCorrectWords + quoteWords[0] + " "
+            
+            for(let i=0; i<wordLength; i++) {
+                correct[i] = ''
+            }
+
+            previousCorrectNum += wordLength+1
+            correctNum -= wordLength+1
+            
+            if (quoteInputElement.value.length == wordLength + 1)
+                quoteInputElement.value = ""
+            
+            else {
+                quoteInputElement.value = quoteInputElement.value.substring(wordLength+1, quoteInputElement.value.length)
+                checkNextWord = true;
+            }
+
+            quoteWords.shift()
+            wordLength = quoteWords[0].length
+            quoteInputElement.setAttribute('maxlength', wordLength+6)
+            totalWordsTyped++
+
+            let info = infoDisplayElement.innerText.split("\n")
+            info[1] = "Words Typed: " + totalWordsTyped
+            infoDisplayElement.innerText = info.join("\n")
+        }
+
+        else if (wordLength == correctNum && quoteWords.length==1) {    // Execute if last correct word.
+            totalWordsTyped++
+            complete = true
+
+            let info = infoDisplayElement.innerText.split("\n")
+            info[1] = "Words Typed: " + totalWordsTyped
+            infoDisplayElement.innerText = info.join("\n")
+        }
+
+        quote = quoteWords.join(" ")
+        console.log(correctNum + previousCorrectNum)
+
+        const TC = document.createElement('span')
+        TC.classList.add('correct')
+        TC.innerText = previousCorrectWords + correct.join("") 
+        
+        const TI = document.createElement('span')
+        TI.classList.add('incorrect')
+        TI.innerText = incorrect.join("")
+
+        const TU = document.createElement('span')
+        TU.classList.add('unTyped')
+        TU.innerText = unTyped.join("")
+
+        quoteDisplayElement.replaceChild(TC, quoteDisplayElement.querySelector('.correct'))
+        quoteDisplayElement.replaceChild(TI, quoteDisplayElement.querySelector('.incorrect'))
+        quoteDisplayElement.replaceChild(TU, quoteDisplayElement.querySelector('.unTyped'))
+    
+        if (complete) {
+            totalSentencesCompleted++
+
+            let info = infoDisplayElement.innerText.split("\n")
+            info[0] = "Sentences Completed: " + totalSentencesCompleted
+            infoDisplayElement.innerText = info.join("\n")
+
+            renderNewQuote()
         }
     }
-
-    if (wordLength+1 <= correctNum && Input[wordLength] == ' ') {
-    
-        previousCorrectWords = previousCorrectWords + quoteWords[0] + " "
-        correct = []
-
-        previousCorrectNum += wordLength+1
-        correctNum = 0
-        
-        if (quoteInputElement.value.length == wordLength + 1)
-            quoteInputElement.value = ""
-        
-        else {
-            quoteInputElement.value = quoteInputElement.value.substring(wordLength+1, quoteInputElement.value.length)
-        }
-
-        
-
-        quoteWords.shift()
-        wordLength = quoteWords[0].length
-        quoteInputElement.setAttribute('maxlength', wordLength+6)
-        totalWordsTyped++
-
-        let info = infoDisplayElement.innerText.split("\n")
-        info[1] = "Words Typed: " + totalWordsTyped
-        infoDisplayElement.innerText = info.join("\n")
-    }
-
-    else if (wordLength == correctNum && quoteWords.length==1) {
-        totalWordsTyped++
-        complete = true
-
-        let info = infoDisplayElement.innerText.split("\n")
-        info[1] = "Words Typed: " + totalWordsTyped
-        infoDisplayElement.innerText = info.join("\n")
-    }
-
-    quote = quoteWords.join(" ")
-
-    const TC = document.createElement('span')
-    TC.classList.add('correct')
-    TC.innerText = previousCorrectWords + correct.join("") 
-    
-    const TI = document.createElement('span')
-    TI.classList.add('incorrect')
-    TI.innerText = incorrect.join("")
-
-    const TU = document.createElement('span')
-    TU.classList.add('unTyped')
-    TU.innerText = unTyped.join("")
-
-    quoteDisplayElement.replaceChild(TC, quoteDisplayElement.querySelector('.correct'))
-    quoteDisplayElement.replaceChild(TI, quoteDisplayElement.querySelector('.incorrect'))
-    quoteDisplayElement.replaceChild(TU, quoteDisplayElement.querySelector('.unTyped'))
-  
-    if (complete) {
-        totalSentencesCompleted++
-
-        let info = infoDisplayElement.innerText.split("\n")
-        info[0] = "Sentences Completed: " + totalSentencesCompleted
-        infoDisplayElement.innerText = info.join("\n")
-
-        renderNewQuote()
-    }
+    while(checkNextWord);
 })
 
 function getRandomQuote() {
